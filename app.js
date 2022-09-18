041ba7583331c74391edfd0f08a81c04ef91ad0c
swapnaliss <swapnalisbhargude123@gmail.com>
2022-09-18 14:15:40 +0530

    feat : server URL store

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ var inputText = document.querySelector("#input-text");
 var outputText = document.querySelector("#output-text");
 var translateButton = document.querySelector("#translate-btn");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
 translateButton.addEventListener("click", clickHandler);
